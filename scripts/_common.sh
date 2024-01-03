aa90c1ca37c552f9d91457fe8980b74ef1925d8d
Félix Piédallu <felix@piedallu.me>
2024-01-03 22:02:22 +0100

    Port to manifestv2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,17 +5,6 @@
 #=================================================
 
 version="2.28.0"
-architecture=""
-case $(uname -m) in
-    i386)    architecture="386" ;;
-    x86_64)  architecture="amd64" ;;
-    armv5)   architecture="armv5" ;;
-    armv6)   architecture="armv6" ;;
-    armv7)   architecture="armv7" ;;
-    armv7l)  architecture="armv7" ;;
-    aarch64) architecture="arm64" ;;
-    *)       ynh_die --message="Sorry, your architecture $YNH_ARCH is not supported." ;;
-esac
 
 #=================================================
 # PERSONAL HELPERS
